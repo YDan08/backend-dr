be6a7571a466d941db53db76708dc2da278b55f8
YDan08 <dasyamashita@gmail.com>
2024-05-24 15:59:04 -0300

    feat: create logic for get quantity ink

--- src/room/resolvers/room.resolver.ts
+++ src/room/resolvers/room.resolver.ts
@@ -1,6 +1,7 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql"
 import { InkModel } from "../models"
 import { CalculateRoomPaintDto } from "../dtos"
+import { roundDecimalPlaces } from "../../utils/round-decimal"
 
 @Resolver()
 export class RoomResolver {
@@ -10,16 +11,57 @@ export class RoomResolver {
 	}
 
 	@Mutation(() => InkModel)
-	async CalculateRoomPaintDto(
+	async CalculateRoomPaint(
 		@Arg("data", () => CalculateRoomPaintDto) data: CalculateRoomPaintDto
 	) {
-		console.log(data)
-		const ink = {
-			ink18: 2,
-			ink3: 3,
-			ink2: 2,
-			ink05: 1,
+		const door = 0.8 * 1.9
+		const window = 2 * 1.2
+
+		const totalMeterRoom = data.walls.reduce((total, currWall) => {
+			const totalWallMeter = currWall.height * currWall.length
+
+			const totalDoor = door * (currWall.quantityDoors ?? 0)
+			const totalWindow = window * (currWall?.quantityWindows ?? 0)
+			if (currWall.quantityDoors && currWall.height < 2.2) {
+				throw new Error("É necessario que a parede tenha uma altura maior!")
+			}
+			if (totalWallMeter / 2 < totalDoor + totalWindow) {
+				throw new Error(
+					"É necessario aumentar o tamanho da parede ou diminuir a quantidade de portas e janelas!"
+				)
+			}
+
+			const wall = totalWallMeter - totalDoor - totalWindow
+
+			return roundDecimalPlaces(total + wall)
+		}, 0)
+
+		let paintRoomLiters = roundDecimalPlaces(totalMeterRoom / 5)
+
+		let ink18 = Math.floor(paintRoomLiters / 18)
+		paintRoomLiters = paintRoomLiters % 18
+		let ink3 = Math.floor(paintRoomLiters / 3.6)
+		paintRoomLiters = paintRoomLiters % 3.6
+		let ink2 = Math.floor(paintRoomLiters / 2.5)
+		paintRoomLiters = paintRoomLiters % 2.5
+		let ink05 = Math.floor(paintRoomLiters / 0.5)
+		paintRoomLiters = paintRoomLiters % 0.5
+
+		if (paintRoomLiters > 0) {
+			ink05 = ink05 + 1
+			return {
+				ink18: ink18,
+				ink3: ink3,
+				ink2: ink2,
+				ink05: ink05,
+			}
 		}
-		return ink
+
+		return {
+			ink18: ink18,
+			ink3: ink3,
+			ink2: ink2,
+			ink05: ink05,
+		}
 	}
 }
