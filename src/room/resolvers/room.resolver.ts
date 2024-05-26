dd3aebcf3062c616d5abcfec19df6282516ab3df
YDan08 <dasyamashita@gmail.com>
2024-05-25 21:59:58 -0300

    fix: change logic of inks

--- src/room/resolvers/room.resolver.ts
+++ src/room/resolvers/room.resolver.ts
@@ -15,17 +15,28 @@ export class RoomResolver {
     const door = 0.8 * 1.9
     const window = 2 * 1.2
 
+    if (data.walls.length !== 4) {
+      throw new Error('É necessario que tenha 4 paredes!')
+    }
+
     const totalMeterRoom = data.walls.reduce((total, currWall) => {
       const totalWallMeter = currWall.height * currWall.length
+
       if (totalWallMeter > 50) {
         throw new Error('A parede não pode ser maior que 50 metros quadrados!')
       }
+
       const totalDoor = door * (currWall.quantityDoors ?? 0)
       const totalWindow = window * (currWall?.quantityWindows ?? 0)
+
       if (currWall.quantityDoors && currWall.height < 2.2) {
         throw new Error('É necessario que a parede tenha uma altura maior!')
       }
-      if (totalWallMeter / 2 < totalDoor + totalWindow) {
+
+      const halfWallArea = totalWallMeter / 2
+      const doorsAndWindowsFitInWall = halfWallArea >= totalDoor + totalWindow
+
+      if (!doorsAndWindowsFitInWall) {
         throw new Error(
           'É necessario aumentar o tamanho da parede ou diminuir a quantidade de portas e janelas!'
         )
@@ -36,34 +47,35 @@ export class RoomResolver {
       return roundDecimalPlaces(total + wall)
     }, 0)
 
-    let paintRoomLiters = roundDecimalPlaces(totalMeterRoom / 5)
+    const totalInk = roundDecimalPlaces(totalMeterRoom / 5)
+    let paintRoomLiters = totalInk
 
-    const ink18 = Math.floor(paintRoomLiters / 18)
-    paintRoomLiters = paintRoomLiters % 18
-    const ink3 = Math.floor(paintRoomLiters / 3.6)
-    paintRoomLiters = paintRoomLiters % 3.6
-    const ink2 = Math.floor(paintRoomLiters / 2.5)
-    paintRoomLiters = paintRoomLiters % 2.5
-    let ink05 = Math.floor(paintRoomLiters / 0.5)
-    paintRoomLiters = paintRoomLiters % 0.5
+    const inkMap = {
+      ink18: 18,
+      ink3: 3.6,
+      ink2: 2.5,
+      ink05: 0.5
+    }
+
+    const result: InkModel = {
+      ink18: 0,
+      ink3: 0,
+      ink2: 0,
+      ink05: 0,
+      totalInk
+    }
+
+    Object.entries(inkMap).forEach(([key, value]) => {
+      const ink = Math.floor(paintRoomLiters / value)
+      paintRoomLiters %= value
+      result[key as keyof InkModel] = ink
+    })
 
     if (paintRoomLiters > 0) {
-      ink05 = ink05 + 1
-      return {
-        ink18: ink18,
-        ink3: ink3,
-        ink2: ink2,
-        ink05: ink05,
-        totalInk: roundDecimalPlaces(totalMeterRoom / 5)
-      }
+      const ink = Math.ceil(paintRoomLiters / 0.5)
+      result.ink05 += ink
     }
 
-    return {
-      ink18: ink18,
-      ink3: ink3,
-      ink2: ink2,
-      ink05: ink05,
-      totalInk: roundDecimalPlaces(totalMeterRoom / 5)
-    }
+    return result
   }
 }
